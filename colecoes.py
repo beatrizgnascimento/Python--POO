5bb0fa07c8474c3d874a74be664982ef3762330b
Beatriz Nascimento <131934165+beatrizgnascimento@users.noreply.github.com>
2023-08-16 16:35:00 -0300

    Add files via upload

---
+++ colecoes.py
@@ -0,0 +1,11 @@
+#Listas são coleções de itens
+nomes = ['Pedro', 'Marina']
+notas = []
+notas.append(9.2) #Adiciona um novo item no final
+print(nomes)
+print(notas)
+print(notas[1]) #coleções iniciam no 0
+
+#arrays tambem sao coleções de itens
+from array import array
+notas = array('d')
